053364673276aa390115b84e928d9412dc123b97
alfianfebiyanto <alfianfebiyanto2@gmail.com>
2025-08-16 07:23:08 +0700

    add load to csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from config.setting import USER_PATH ,PRODUCT_PATH, TRANSCATION_PATH
+from config.setting import USER_PATH ,PRODUCT_PATH, TRANSCATION_PATH ,OUTPUT_PATH
 from scripts.ekstract import exctract_csv, extract_json
 from scripts.transform import transform_data
+from scripts.load import save_to_csv
 
 # Extract
 product_df = exctract_csv(PRODUCT_PATH)
@@ -9,3 +10,6 @@ user_df = extract_json(USER_PATH)
 
 # Transform
 final_df = transform_data(product_df,transaction_df,user_df)
+
+# Load 
+save_to_csv(final_df, OUTPUT_PATH)
